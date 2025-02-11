a15288c95e05d8c8dadb94463b62dae352ca35b5
halee-t <haleetisler@gmail.com>
2025-02-10 23:45:30 -0500

    working on packaging

--- frontend/src/electron/main.ts
+++ frontend/src/electron/main.ts
@@ -1,8 +1,6 @@
 import { app, BrowserWindow } from "electron";
 import path from "path"; //helps cross-platform
 
-type test = string;
-
 //waiting for event on the app
 app.on("ready", () => {
   const mainWindow = new BrowserWindow({});
