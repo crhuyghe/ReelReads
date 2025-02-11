371212b6697709d4bb49b62d1c5c887f5324dbc1
halee-t <haleetisler@gmail.com>
2025-02-10 23:08:57 -0500

    electron installation and setup

---
+++ frontend/src/electron/main.js
@@ -0,0 +1,8 @@
+import { app, BrowserWindow } from "electron";
+import path from "path"; //helps cross-platform
+
+//waiting for event on the app
+app.on("ready", () => {
+  const mainWindow = new BrowserWindow({});
+  mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html")); //app.getAppPath accounts for not knowing where the user will place the app
+});
