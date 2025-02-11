113bbeadc417fe9d3484d7c9cd160985dccbabda
halee-t <haleetisler@gmail.com>
2025-02-10 23:24:23 -0500

    allowing use of ts files

--- frontend/src/electron/main.ts
+++ frontend/src/electron/main.ts
@@ -1,6 +1,8 @@
 import { app, BrowserWindow } from "electron";
 import path from "path"; //helps cross-platform
 
+type test = string;
+
 //waiting for event on the app
 app.on("ready", () => {
   const mainWindow = new BrowserWindow({});
