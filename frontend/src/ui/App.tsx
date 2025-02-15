5c7333be8646c68394a97ab17e987876f0443d82
halee-t <haleetisler@gmail.com>
2025-02-15 17:25:18 -0500

    adding tailwind configuration

--- frontend/src/ui/App.tsx
+++ frontend/src/ui/App.tsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import "./index.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <div className="bg-red-500 text-blue-300">testing live 123</div>
       <div>
-        testing live 123
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,5 +12,10 @@ export default defineConfig({
   server: {
     port: 5123,
     strictPort: true,
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
   },
 });
