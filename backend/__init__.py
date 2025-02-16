044baf22850ff0a10c28f8435e31ac4d1e8a0e2f
Christian Huyghe <huyghecr@gmail.com>
2025-02-16 13:27:55 -0500

    Setting recommendation system structuring

---
+++ backend/__init__.py
@@ -0,0 +1,1 @@
+from .RecommendationManager import RecommendationManager
---
+++ main.py
@@ -0,0 +1,7 @@
+from backend import RecommendationManager
+
+rm = RecommendationManager()
+
+print(rm.search_by_book(862267))
+# print(rm.search_by_movie(671))
+
