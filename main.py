5cca30b397920717d51ce2ca6b0f5f1564ec36e9
Christian Huyghe <huyghecr@gmail.com>
2025-02-16 15:08:51 -0500

    Added user history manager class

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ from backend import HistoryManager
 
 hm = HistoryManager(0)
 
-hm.initialize_history({"movie": {671: 5.0, 123: 1.0}, "book": {862267: 5.0, 659469: 1.0}})
+hm.initialize_history({"movie": {671: (5.0, 0), 123: (1.0, 0)}, "book": {862267: (5.0, 0), 659469: (1.0, 0)}})
 
 print(hm.sentiment_vector)
