d3b43b00ad83da1902c95e4d2e264b126225ae84
halee-t <haleetisler@gmail.com>
2025-02-18 22:31:08 -0500

    adding navigation back to login for quick testing

--- frontend/src/ui/components/Welcome.tsx
+++ frontend/src/ui/components/Welcome.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import Login from "./Login";
 
 const Welcome = () => {
-  return <div>CONGRATS YOU SIGNED IN!!!!</div>;
+  const [currentComponent, setCurrentComponent] = useState("welcome");
+
+  const handleClick = (component: string) => {
+    setCurrentComponent(component);
+  };
+
+  return (
+    <div>
+      {currentComponent === "welcome" ? (
+        <div>
+          CONGRATS YOU SIGNED IN!!!!
+          <button onClick={() => handleClick("login")}>Back to login</button>
+        </div>
+      ) : (
+        <Login />
+      )}
+    </div>
+  );
 };
 
 export default Welcome;
