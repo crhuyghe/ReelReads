00190d841c4f85548cbc669973407f1e10104f56
Christian Huyghe <huyghecr@gmail.com>
2025-03-05 12:49:38 -0500

    Trimmed movie ratings

--- database/movie_data/recombine.py
+++ database/movie_data/recombine.py
@@ -2,4 +2,4 @@ import pandas as pd
 import os
 
 df = pd.concat([pd.read_csv(file) for file in os.listdir() if "ratings_trimmed" in file and file != "ratings_trimmed.csv"], ignore_index=True)
-df.to_csv("ratings_trimmed.csv", index=False)
+df.to_csv("ratings_trimmed_full.csv", index=False)
