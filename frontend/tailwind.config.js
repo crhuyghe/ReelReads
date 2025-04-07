b991eb35ad23fc1fc0128c39e2c40d7b14ce699f
halee-t <haleetisler@gmail.com>
2025-04-06 20:03:56 -0400

    adding app colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,17 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: ["./index.html", "./src/ui/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-};
-
-module.exports = {
-  darkMode: "class",
-  content: ["./src/ui/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#81B1D1",
+        secondary: "#4E7D9C",
+        secondary_light: "#D9F0F8",
+        accent: "#FFBC70",
+        brand: {
+          light: "#FBF9E3",
+          dark: "#0D263E",
+        },
+      },
+    },
   },
   plugins: [],
 };
